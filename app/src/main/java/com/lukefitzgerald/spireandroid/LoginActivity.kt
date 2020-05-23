377ec4a6f79e39d6b3f2d8b35f9b9601f46836ab
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-05-23 16:57:00 -0600

    adding new activity, navigating to that new activity

--- app/src/main/java/com/lukefitzgerald/spireandroid/LoginActivity.kt
+++ app/src/main/java/com/lukefitzgerald/spireandroid/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.lukefitzgerald.spireandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        emailInput = findViewById(R.id.email_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            // Login (Showing the main activity intent)
+            val intent = Intent(this, MainApplicationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/lukefitzgerald/spireandroid/MainApplicationActivity.kt
@@ -0,0 +1,12 @@
+package com.lukefitzgerald.spireandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainApplicationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_application)
+    }
+}
