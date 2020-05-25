2a7989b51f11e786c97120761a7dd3c2ab35dda1
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-05-25 13:24:26 -0600

    adding one unit test, just to figure out how to run them

---
+++ app/src/test/java/com/lukefitzgerald/spireandroid/views/models/CustomerListViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.lukefitzgerald.spireandroid.views.models
+
+import com.lukefitzgerald.spireandroid.models.Customer
+import com.lukefitzgerald.spireandroid.repositories.CustomerRepository
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class CustomerListViewModelTest {
+
+//    private lateinit var customer: Customer
+//    private lateinit var customerRepository: CustomerRepository
+//    private lateinit var subject: CustomerListViewModel
+//
+//    @Before
+//    fun setUp() {
+//        customer = Customer()
+//        customer.firstName = "Test"
+//        customer.lastName = "Customer"
+//
+//        customerRepository = CustomerRepository()
+//        subject = CustomerListViewModel()
+//    }
+
+    @Test
+    fun testUnitTest() {
+        assertEquals(1, 1)
+    }
+}
